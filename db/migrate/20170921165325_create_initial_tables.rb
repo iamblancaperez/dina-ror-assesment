6f144b8a48301b6711b50616ab65d5b3f6c062ca
Blanca Perez <blankp@gmail.com>
2017-09-22 11:23:51 -0300

    Added initial tables migrations

--- db/migrate/20170921165325_create_initial_tables.rb
+++ db/migrate/20170921165325_create_initial_tables.rb
@@ -1,6 +1,32 @@
 class CreateInitialTables < ActiveRecord::Migration[5.1]
   def change
-    create_table :initial_tables do |t|
+    create_table :users do |t|
+      t.string :name
+      t.string :status
+      t.timestamps
     end
+
+    create_table :roles do |t|
+      t.string :name
+      t.timestamps
+    end
+
+    create_table :permissions do |t|
+      t.string :name
+      t.belongs_to :role, index: true
+      t.belongs_to :user, index: true
+      t.timestamps
+    end
+
   end
+  def self.down
+  end
+
+  end
+
+
+roles have many permissions
+users have many permissions
+users have only one roles
+
 end
